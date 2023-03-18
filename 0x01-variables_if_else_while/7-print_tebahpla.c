625f610cf9fb93fea0d694ca27dc981b894a916d
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-17 17:52:16 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -main
+ * Descr - reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	puchar('\n');
+	return (0);
+}
