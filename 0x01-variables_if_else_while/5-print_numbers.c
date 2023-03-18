811552b110d6e986befb568bece7bf58b491231d
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-17 17:41:17 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Descr - Print base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0 ; i < 10 ; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
