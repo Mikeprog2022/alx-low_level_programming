c149181b43b5179f6ff98feb69cd7f72fa0c7cdd
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-21 16:53:45 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * main - Print _putchar
+ * Return: 0
+ * Descr: Printing without std libs
+ */
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; x[i] =! '\0'; i++)
+	{
+		_putchar(x[i]);
+	}
+	putchar('\n');
+	return (0);
+}
