a2c9b5e63826b51497acf3ed35897320a3a5d6df
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-21 18:21:37 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,25 @@
 #include "main.h"
+/**
+ * print_sign - Prints sign of num
+ * @n - int
+ * Description- Prints sign
+ * Return: 0/1/-1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
