e6275c5ac6af374d4dec439575be470edb2f725f
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-21 17:59:56 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ * _islower - Return true if lower
+ * Descr - Return 1 if lower
+ * @c: char
+ * Return: 0/1
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
