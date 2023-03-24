2a68f34bb1c06b7eb7b7b7195e60569a3d21c008
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-24 15:23:50 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - Dont print 2 and 4
+ * i: int
+ * Return: 0-9 wihtout 2,4
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
