4fb9d854fc9bc766cb28a6af831bd15a8bfeed67
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-24 15:31:19 -0700

    task4 fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (i <= 9)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
+		i++;
 	}
 	_putchar('\n');
 }
