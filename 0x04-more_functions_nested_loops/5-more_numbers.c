af887625b47c2b56c5c7cb35007101b030c104ec
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-24 15:45:09 -0700

    task5 fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,18 @@
  */
 void more_numbers(void)
 {
-	int x;
+	int x = 0;
 	int i;
 
-	while (x <= 10)
+	while (x < 10)
 	{
 		for (i = 0; i <= 14; i++)
 		{
+			if (i > 9)
+			{
 			_putchar(i + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
 		x++;
 		_putchar('\n');
