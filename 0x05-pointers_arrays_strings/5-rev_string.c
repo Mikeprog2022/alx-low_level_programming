6cc5c970fef6b1e403b6fdc4f0633553d252d43e
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-28 16:08:15 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,16 @@
  */
 void rev_string(char *s)
 {
+	int len = 0;
+	int x = 0;
+	char y;
 
+	while (s[len] != '\0')
+		len++;
+	while (x < len--)
+	{
+		y = s[x];
+		s[x++] = s[len];
+		s[len] = y;
+	}
 }
