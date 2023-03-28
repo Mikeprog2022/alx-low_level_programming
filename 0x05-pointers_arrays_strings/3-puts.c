5bf392f5de08cfc034567f0025f298ab8dfe059a
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-28 15:41:11 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - Prints string
+ * @str: pointer
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
