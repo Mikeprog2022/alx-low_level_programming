8914ac73d05ba09c1cc88a5d1ee61af50947239f
Mikeprog2022 <mikeprog22@gmail.com>
2023-03-28 16:10:40 -0700

    task4.2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,11 @@ void print_rev(char *s)
 {
 	int x;
 
-	while (s[x] != '\0')
-	{
-		x++;
-	}
-	for (x -= 1; x >= 0; x--)
-	{
+	for (x = 0; s[x] != '\0'; x++)
+		;
+
+	for (x--; x >= 0; x--)
 		_putchar(s[x]);
-	}
+
 	_putchar('\n');
 }
